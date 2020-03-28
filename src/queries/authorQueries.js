0ec43836797c25b1e78ef9a4de758ea523df2c5c
Aishwaryakotharu <44223350+Aishwaryakotharu@users.noreply.github.com>
2020-03-28 11:47:40 +0530

    Initial commit

---
+++ src/queries/authorQueries.js
@@ -0,0 +1,52 @@
+require("../config/mongodb");
+
+const Author = require("../models/author");
+const Book = require("../models/book");
+
+const newAuthor = new Author({
+  first_name: "Albert",
+  last_name: "Einstein",
+  date_of_birth: new Date(1879, 2, 14),
+  field_of_study: "non-fiction",
+  date_of_death: new Date(1995, 3, 18)
+});
+
+// 5e7ed38f2ea8f8033c488d41,
+
+const newBook = new Book({
+  title: "The world as I see it",
+  author: "5e7ed38f2ea8f8033c488d41",
+  summary: "Albert Einstein believes in humanity, in a peaceful world of mutual helpfulness, and in the high mission of science. This book is intended as a plea for this belief at a time which compels every one of us to overhaul his mental attitude and his ideas.",
+  isbn: "978-0806527901"
+});
+
+newBook.save()
+  .then(response => {
+    console.log(response);
+  })
+  .catch(console.error)
+
+ newAuthor.save()
+ .then(response => {
+   console.log(response);
+  })
+  .catch(console.error);
+/*
+  Author.findById("5e7eead262ae8203b336c38d")
+   .then(console.log)
+   .catch(console.error)
+*/
+/*
+   Author.findByIdAndUpdate("5e7eead262ae8203b336c38d", {
+     $set: {
+      first_name: "Stephen",
+      last_name: "Hawking"
+     }
+    })
+   .then(console.log)
+   .catch(console.error)
+
+   Author.find({first_name: "Stephen"})
+  .then(console.log)
+   .catch(console.error)
+    */
---
+++ src/queries/bookQueries.js
@@ -0,0 +1,19 @@
+const Book = require("../models/book");
+
+const newBook = new Book({
+  title: "The world as I see it",
+  author: "5e7ed38f2ea8f8033c488d41",
+  summary: "Albert Einstein believes in humanity, in a peaceful world of mutual helpfulness, and in the high mission of science. This book is intended as a plea for this belief at a time which compels every one of us to overhaul his mental attitude and his ideas.",
+   isbn: "978-0806527901"
+ });
+
+newBook.save()
+.then(response => {
+    console.log(response);
+   })
+
+   Book
+  .findById("5e7eead262ae8203b336c38d")
+  .populate("author", "first_name last_name")
+  .then(console.log)
+  .catch(console.error)
